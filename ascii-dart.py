c49b89999e02d9010ac8fc02194cfce5b3b333c8
olafurfo <olafuro21@ru.is>
2022-11-24 15:40:58 +0000

    Dart updated, maybe frontpage ?

--- ascii-dart.py
+++ ascii-dart.py
@@ -1,13 +1,23 @@
+import time
 
-
-def print_dart():
+def get_dart_list():
     with open("dart.txt") as file_obj:
-        dart_list = [line for line in file_obj]
-    for element in dart_list:
-        print(element, end="")
-    print()
+        return [line.replace("\n", "") for line in file_obj]
     
+dart = get_dart_list()
+for i, line in enumerate(dart):
+    if i < 16:
+        time.sleep(0.1)
+        print(line)
+    elif i >= 16 and i < 29:
+        time.sleep(0.3)
+        print(line)
+    else:
+        time.sleep(0.05)
+        print(line)
 
-    
-
-print_dart()
+input("Press enter")
+print()
+print()
+print()
+print("JEEEEBOOOOYYYYYY")
