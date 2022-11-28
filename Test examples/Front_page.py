7c0c7989d1d9603accaf9abcba65dfd9f787be11
olafurfo <olafuro21@ru.is>
2022-11-28 14:01:05 +0000

    Basic files initialized

--- Test examples/Front_page.py
+++ Test examples/Front_page.py
@@ -1,7 +1,7 @@
 import time
 
 def print_frontpage():
-    with open("dart.txt") as file_obj:
+    with open("Test examples/dart.txt") as file_obj:
         dart =  [line.replace("\n", "") for line in file_obj]
     for i, line in enumerate(dart):
         if i < 16:
