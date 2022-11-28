899e64e21db3b2d125d87593f2d57983908ba1fd
olafurfo <olafuro21@ru.is>
2022-11-28 13:52:36 +0000

    simple usertests

---
+++ Test examples/Front_page.py
@@ -0,0 +1,18 @@
+import time
+
+def print_frontpage():
+    with open("dart.txt") as file_obj:
+        dart =  [line.replace("\n", "") for line in file_obj]
+    for i, line in enumerate(dart):
+        if i < 16:
+            time.sleep(0.1)
+            print(line)
+        elif i >= 16 and i < 29:
+            time.sleep(0.3)
+            print(line)
+        else:
+            time.sleep(0.05)
+            print(line)
+    input("Please press enter")
+
+print_frontpage()
