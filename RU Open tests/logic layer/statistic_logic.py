e57f0746e2557b3d44f2c59552cdb7e428d619b9
thorarinnh <thorarinn22@ru.is>
2022-11-29 15:41:05 +0000

    logic

---
+++ RU Open tests/logic layer/statistic_logic.py
@@ -0,0 +1,19 @@
+
+class Statistic_Logic:
+    def get_win_percentage(self):
+        pass
+
+    def get_quality_points(self):
+        pass
+
+    def get_games_played(self):
+        pass
+
+    def view_stats_for_everything(self):
+        pass
+
+    def view_player_stats():
+        pass
+
+    def view_team_stats():
+        pass
---
+++ RU Open tests/logic layer/team_logic.py
@@ -0,0 +1,35 @@
+
+
+class Team_Logic:
+    def __init__(self):
+        pass
+
+    def validate_team_entry(self):
+        pass
+    
+    def get_all_teams(self):
+        pass
+
+    def get_team_stats(self):
+        pass
+
+    def get_team(self):
+        pass
+
+    def update_team(self):
+        pass
+
+    def create_team(self):
+        pass
+    
+    def edit_team(self):
+        pass
+
+    def view_team(self):
+        pass
+
+    def view_team_players(self):
+        pass
+
+    def delete_team(self):
+        pass
