e50dc67386c6f79f434ef2a296f7508c878f5f9b
Gunterinn <98gunnarhelgi98@gmail.com>
2022-11-30 09:49:38 +0000

    Færði Logic classes frá test folder í hitt test folderið :+1:

---
+++ RU_Open_tests/logic_layer/association_logic.py
@@ -0,0 +1,22 @@
+class association_logic:
+    def validate_association_entry(self):
+        pass
+    
+    def update_association(self):
+        pass
+
+    def view_association(self):
+        pass
+
+    def delete_association(self):
+        pass
+
+    def get_all_associations(self):
+        pass
+
+    def get_association(self):
+        pass
+    
+    def edit_association(self):
+        pass
+
