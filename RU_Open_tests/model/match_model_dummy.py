6863cae611da255f407f9f13ded0e90a425bbca0
JakobMatt1310 <jakobm22@ru.is>
2022-12-01 15:48:25 +0000

    seinustu breytingar dagsins og skrif í dagbók

--- RU_Open_tests/model/match_model_dummy.py
+++ RU_Open_tests/model/match_model_dummy.py
@@ -1,10 +1,50 @@
 from model.game_model_dummy import Game
 
 class Match:
-    def __init__(self, home_team="", away_team="", match_scheduled="", games=None):
+    def __init__(self, home_team="", away_team="", match_scheduled="", game_score=""):
         self.home_team = home_team
         self.away_team = away_team
         self.match_scheduled = match_scheduled
+        self.game_score = game_score
 
     def __get_games__():
         pass
+
+
+    #Match = directory
+    #key: game1 values:[home_team_player][away_team_player][home_leg_wins = 1, 0][away_leg_wins = 1, 1]
+    #example of entry
+    #create_match --->
+    #Please enter the home_team --->
+    #Please enter the away_team --->
+    #print("The four first games will be played with 501 gamerules with one player.")
+    # Team Name
+    
+    # player1
+    # player2
+    # player3
+    # player4
+    
+    #input("Please select a player from home_team_player_list to compete in the first 501 singles game")
+    
+    # Team Name
+    
+    # player1
+    # player2
+    # player4
+
+    #input("Please select a player from home_team_player_list to compete in the second 501 singles game")
+
+    # Team Name
+    
+    # player1
+    # player4
+
+    #input("Please select a player from home_team_player_list to compete in the third 501 singles game")
+
+    #print("so player1 will play in the final 501 singles game!")
+
+
+    
+
+
