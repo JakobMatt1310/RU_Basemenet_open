8fbb22b8575c08203393b29cc2508c9e45fa021a
olafurfo <olafuro21@ru.is>
2022-12-02 16:28:11 +0000

    main_menu uptade
    Possible to create team and player
    id's are valid
    need to add role to player

--- RU Open/Model/team.py
+++ RU Open/Model/team.py
@@ -3,7 +3,9 @@ class Team:
 
 
 
-    def __init__(self, team_name="", association_name="", captain_name=""):
+    def __init__(self, id="", association_id="", team_name="", association_name="", captain_name=""):
+        self.id = id
+        self.association_id = association_id
         self.team_name = team_name
         self.association_name = association_name
         self.captain_name = captain_name
