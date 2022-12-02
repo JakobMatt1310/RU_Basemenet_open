343f9ac7f6dc759d63a65d9a81c2213b796b6159
JakobMatt1310 <jakobm22@ru.is>
2022-12-02 14:40:44 +0000

    breytingar við teams_ui

--- RU Open/UI/moderator_menu/team_menu/teams_ui.py
+++ RU Open/UI/moderator_menu/team_menu/teams_ui.py
@@ -2,7 +2,7 @@ from model.player import Player
 from logic.logic_wrapper import Logic_Wrapper
 from print_layouts import print_current_menu
 from model.team import Team
-from player_logic import Player_Logic
+from logic.player_logic import Player_Logic
 from input_validators import *
 
 class Teams_UI:
@@ -38,35 +38,34 @@ class Teams_UI:
                 team.association_name = input("Enter the name of the association this team should belong to: ")
                 team.captain_name = input("Enter the name of the teams captain: ")
                 self.logic_wrapper.create_team(team)
-                ask_to_create_players = input("Would you like to add players to this team(yes/no)?: ")
+                ask_to_create_players = input("Would you like to add players to this team? (yes/no): ")
                 if ask_to_create_players == 'yes':
                     add_another = 'yes'
                     while add_another == 'yes':
-                        yes_or_no = input("Would you like create a new player? (yes/no): ")
-                        if yes_or_no == 'yes':
-                            player = Player()
-                            while True:
-                                player.name = input("Enter the name of the player: ")
-                                try:
-                                    validate_name(player.name)
-                                    break
-                                except NameLengthException:
-                                    print("name was too long")
-                                except:
-                                    print("some error")
-                            player.ssn = input("Enter the social security number of the player: ")
-                            player.phone = input("Enter the players phone number: ")
-                            player.email = input("Enter the players email address: ")
-                            player.address = input("Enter the players home address: ")
-                            player.team_id = team.id
-                            self.logic_wrapper.create_player(player)
-                        else:
-                            print("Invalid input, try again")
+                        print("Creating a new player...\n")
+                        player = Player()
+                        while True:
+                            player.name = input("Enter the name of the player: ")
+                            try:
+                                validate_name(player.name)
+                                break
+                            except NameLengthException:
+                                print("name was too long")
+                            except:
+                                print("some error")
+                        player.ssn = input("Enter the social security number of the player: ")
+                        player.phone = input("Enter the players phone number: ")
+                        player.email = input("Enter the players email address: ")
+                        player.address = input("Enter the players home address: ")
+                        player.team_id = team.id
+                        self.logic_wrapper.create_player(player)
+                        add_another = input("Would you like to add another player? (yes/no): ")
+
                 elif ask_to_create_players == 'no':
                     pass
-            elif command == "3":
-                result = self.logic_wrapper.get_all_teams()
-                view_teams(result)
-                return "q"
+            #elif command == "3":
+            #    result = self.logic_wrapper.get_all_teams()
+            #    view_teams(result)
+            #    return "q"
             else:
                 print("invalid input, try again")
