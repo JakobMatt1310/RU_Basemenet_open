03815a0cbd223ef6e06bf8f1a933e7ddfbac65b0
Hjortur Helgi <hjorturt21@ru.is>
2022-12-05 11:32:53 +0000

    .
    .

--- RU Open/Model/team.py
+++ RU Open/Model/team.py
@@ -2,17 +2,16 @@
 class Team:
 
     def __init__(self, id="", association_id="", team_name="", association_name="", captain_name=""):
+        '''Constructor for the Team class'''
+
         self.id = id
         self.association_id = association_id
         self.team_name = team_name
         self.association_name = association_name
         self.captain_name = captain_name
 
+
     def __str__(self):
         '''Returns a string representation of the Team object'''
-        # team_str = ""
-        team_str = "".join(i for i in self.team_players[:])
-        # for player in self.team_players[:]:
-        #     return_team_list += f'{str(player)}'
 
-        return f"Team: {self.team_name:>5}, Association: {self.team_association_name:>5}, Captain: {self.team_captain_name:>5}, Team members: {team_str:>5}"
+        return f"Team: {self.team_name:>5}, Association: {self.association_name:>5}, Captain: {self.captain_name:>5}"
