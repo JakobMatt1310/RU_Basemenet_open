8b29e0a3cbf5e931d4caf59f3bb79c3400601ebb
Hjortur Helgi <hjorturt21@ru.is>
2022-12-05 11:54:48 +0000

    change
    mode.team.py

--- RU Open/Model/team.py
+++ RU Open/Model/team.py
@@ -1,17 +1,17 @@
 
 class Team:
 
-    def __init__(self, id="", association_id="", team_name="", association_name="", captain_name=""):
+    def __init__(self, id="",team_name="", association_name="", association_id="", captain_name=""):
         '''Constructor for the Team class'''
 
         self.id = id
-        self.association_id = association_id
         self.team_name = team_name
         self.association_name = association_name
+        self.association_id = association_id
         self.captain_name = captain_name
 
 
     def __str__(self):
         '''Returns a string representation of the Team object'''
 
-        return f"Team: {self.team_name:>5}, Association: {self.association_name:>5}, Captain: {self.captain_name:>5}"
+        return f"ID: {self.id:>5}, Team: {self.team_name:>5}, Association: {self.association_name:>5}, Association ID: {self.association_id:>5}, Captain: {self.captain_name:>5}"
