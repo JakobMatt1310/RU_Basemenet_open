f0cb089ead80110642fff69606454b6116cd32c1
Hjortur Helgi <hjorturt21@ru.is>
2022-12-06 10:41:40 +0000

    tournament changes

--- RU Open/Model/tournament.py
+++ RU Open/Model/tournament.py
@@ -1,24 +1,24 @@
 
 class Tournament():
 
-    def __init__(self, tour_name="", tour_address="", start_date="", end_date="", game_count="", teams_submitted="", game_id="", player_nr="", game_type="", leg_nr="", player_id="", q_points=""):
+    def __init__(self, tour_name="", tour_address="", start_date="", end_date="", game_count="", teams_submitted="", game_id="", player_nr="", game_type="", leg_nr="", player_id=""):
         '''Constructor for the Tournament class'''
 
-        self.tour_name = tour_name
-        self.tour_address = tour_address
-        self.start_date = start_date
-        self.end_date = end_date
-        self.game_count = game_count
-        self.teams_submitted = teams_submitted
-        self.game_id = game_id
-        self.player_nr = player_nr
-        self.game_type = game_type
-        self.leg_nr = leg_nr
-        self.player_id = player_id
-        self.q_points = q_points
+        self.tour_name = tour_name              # Name of the tournament
+        self.tour_address = tour_address        # Address of the tournament
+        self.start_date = start_date            # Starting date of the tournament
+        self.end_date = end_date                # End date of the tournament
+        self.game_count = game_count            # Number of games played in the tournament
+        self.teams_submitted = teams_submitted  # Number of teams submitted to the tournament
+        self.game_id = game_id                  # Game ID
+        self.player_nr = player_nr              # Player number are somehow paired with the player ID and picked by random to play apposite teams players
+        self.game_type = game_type              # Game type 7 games in whole, 501 1vs1 4 games, 301 2vs2 1 game, Cricket 2vs2 1 game, 501 4vs4 1 game ==== 7 games in whole
+        self.leg_nr = leg_nr                    # Leg number is "round" in each game, there is always 3 legs in each game and winner is the one who wins 2 legs or best of 3
+        self.player_id = player_id              # Player ID is the ID of the player in the tournament
+        # self.q_points = q_points                # Q points (B-krafa)
 
     
     def __str__(self):
         '''Returns a string representation of the tournament object'''
 
-        return f"Tournament name: {self.tour_name:>5}, Address: {self.tour_address:>5}, Starting date: {self.start_date:>5}, End_date: {self.end_date:>5}, Number of games played: {self.game_count:>5}, Number of teams submitted to tournament: {self.teams_submitted:>5}, Game ID: {self.game_id:>5}, Player number: {self.player_nr:>5}, Game type: {self.game_type:>5}, Leg number: {self.leg_nr:>5}, Player ID: {self.player_id:>5}, Q points: {self.q_points:>5}"
+        return f"Tournament name: {self.tour_name:>5}, Address: {self.tour_address:>5}, Starting date: {self.start_date:>5}, End_date: {self.end_date:>5}, Number of games played: {self.game_count:>5}, Number of teams submitted to tournament: {self.teams_submitted:>5}, Game ID: {self.game_id:>5}, Player number: {self.player_nr:>5}, Game type: {self.game_type:>5}, Leg number: {self.leg_nr:>5}, Player ID: {self.player_id:>5}"
