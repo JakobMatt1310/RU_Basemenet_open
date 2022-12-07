441af5e56412ee254878c94c52c35b461b3a9ffe
Gunterinn <gunnarb22@ru.is>
2022-12-07 14:14:36 +0000

    Logic-Layer

--- RU_Open_tests/logic_layer/association_logic.py
+++ RU_Open_tests/logic_layer/association_logic.py
@@ -1,12 +1,18 @@
 from data_layer.associations_data import Associations_Data
-from model.associations import Associations
+from model.association_model_dummy import Associations
 
 class Association_Logic:
     def __init__(self, data_connection):
         self.data_wrapper = data_connection
 
     def create_association(self, association):
-        self.data_wrapper.create_association(association)
+        associations = self.data_wrapper.create_association(self, association)
+        association_input = association
+        if association_input in associations:
+            return ValueError
+        else:
+            self.data_wrapper.create_association(self, association)
+            return True
     
     def update_association(self):
         self.data_wrapper.update_association()
@@ -25,17 +31,23 @@ class Association_Logic:
             ret_dict.update(tmp_dict)
         return ret_dict
 
-    def get_all_associations(self, associations):
-            
+    def get_all_associations(self):
         return self.data_wrapper.get_all_associations()
 
-    def get_association(self, association):
-        get_association = self.get_association()
-        association = []
-        for p in get_association:
-            if p.association_id == association.id:
-                association.append(p)
-        return association
+    def validate_association_name_with_all(self, name):
+        all_associations = self.get_all_associations()
+        for association in all_associations:
+            if name == association.association_name:
+                return True
+        return None
+
+    def get_association(self):
+        get_association = self.data_wrapper.get_all_associations()
+        association_input = ???????
+        if association_input in get_association:
+            return association_input
+        else:
+            return ValueError
     
     def edit_association(self):
         self.data_wrapper.edit_association()
