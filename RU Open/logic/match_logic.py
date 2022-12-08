a548cafde365b1a8090d2640b132bbc97364b573
olafurfo <olafuro21@ru.is>
2022-12-08 20:59:09 +0000

    Match_data and layer added

---
+++ RU Open/logic/match_logic.py
@@ -0,0 +1,17 @@
+
+
+class Match_logic:
+    def __init__(self, data_connection):
+        self.data_wrapper = data_connection
+        
+    def get_all_matches(self):
+        return self.data_wrapper.get_all_matches()
+
+    def create_match(self, match):
+        return self.data_wrapper.create_match(match)
+    
+    def update_match(self, match):
+        return self.data_wrapper.update_match(match)    
+
+
+    
