5f5ff946db15c7090811eb0bfcd7e6d3900e34a1
JakobMatt1310 <jakobm22@ru.is>
2022-12-08 17:52:43 +0000

    Tournament UI og Tournament_Edit_UI

--- RU Open/ui/moderator_menu/tournament_menu/tournament_ui.py
+++ RU Open/ui/moderator_menu/tournament_menu/tournament_ui.py
@@ -30,30 +30,75 @@ class Tournament_UI:
                 view_tournaments(tournaments_all)
 
             elif command == "2":
-                tournament = Tournament()
-
-                while True:
-                    tournament.tournament_name = input("Enter the name of the tournament: ")
-                    tournament.tournament_address = input("Enter the address of the tournament: ")
-                    try:
-                        validate_tournament_name(tournament.tournament_name)
-                        validate_home_address(tournament.tournament_address)
-                        break
-                    except TournamentNameLengthException:
-                        print("name was too long, try again")
-                    except HomeAddressException:
-                        print("The address is invalid, try again")
-                    except:
-                        print("Something went wrong, please try again")
-                
-                self.logic_wrapper.create_tournament(tournament)
-
+                self.create_new_tournament()
+            
             elif command == "3":
                 print("edit the tournament")
                 pass
 
             elif command == "4":
+                # prenta lista af tournaments sem eru til en ekki búið að skrá neinar niðurstöður a.k.a. tournaments sem má eyða.
                 self.logic_wrapper.remove_tournament(tournament)
                 
             else:
                 print("invalid input, try again")
+
+    def create_new_tournament(self):
+        tournament = Tournament()
+        return_command = ""
+
+        while True:
+            tournament.tournament_name = input("Enter the name of the tournament: ")
+            tournament.tournament_address = input("Enter the address of the tournament: ")
+            tournament.start_date = input("Please enter the first day of the tournament (dd.mm.yyyy): ")
+            tournament.end_date = input("Now enter the final day of the tournament (dd.mm.yyyy): ")
+            tournament.organizer = input("Enter the name of the tournament organizer: ")
+            tournament.organizer_number = input("Please enter a phone number where the organizer may be reached")
+            try:
+                validate_tournament_name(tournament.tournament_name)
+                validate_home_address(tournament.tournament_address)
+                validate_phonenumber(tournament.organizer_number)
+                break
+            except TournamentNameLengthException:
+                print("name was too long, try again")
+            except HomeAddressException:
+                print("The address is invalid, try again")
+            except PhoneNumberException:
+                print("Invalid phonenumber (must be 7 digits long), please try again.")
+            except:
+                print("Something went wrong, please try again")
+        
+        self.logic_wrapper.create_tournament(tournament)
+        ask_add_teams = input("Would you like to add teams to the tourney right away? (yes/no): ")
+        if ask_add_teams == 'yes':
+            self.add_teams(tournament.id)
+        else:
+            return
+    
+    def add_teams(self, tournament_id):
+        add_another = 'yes'
+        while add_another == 'yes':
+            team_to_add = input("Please enter the name of the team you want to add to the tournament: ")
+            teams_list = self.logic_wrapper.get_teams_by_name(team_to_add)
+            
+            id_list = []
+            print("{:<18}{:<20}{}".format("Team ID", "Team Name"))
+            for team in teams_list:
+                print("{:<15}{:<17}".format(team.id, team.name))
+                id_list.append(team.id)
+            if len(id_list) == 1:
+                selection = id_list[0]
+                team_to_add = self.logic_wrapper.get_team(selection)
+                self.logic_wrapper.add_team_to_tourney(tournament_id, team_to_add.id)
+                add_another = ("Would you like to add another team? (yes/no): ")
+            else:  
+                selection = input("Select team by id: ")
+                if selection == 'b':
+                    print("Going back")
+                    return
+                elif selection in id_list:
+                    team_to_add = self.logic_wrapper.get_team(selection)
+                    break
+                else:
+                    ("The team id you entered is invalid, please try again.")
+
