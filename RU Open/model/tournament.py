0a4411bb6e58211140c43215815e1a26b824b2f7
olafurfo <olafuro21@ru.is>
2022-12-08 23:31:37 +0000

    Tournament updated

--- RU Open/model/tournament.py
+++ RU Open/model/tournament.py
@@ -1,13 +1,13 @@
 class Tournament():
-    def __init__(self, id="", tournament_name="", tournament_address="", start_date="", end_date="", organizer="", organizer_number=""):
+    def __init__(self, id="", name="", address="", start_date="", end_date="", organizer="", organizer_number=""):
         '''Constructor for the Tournament class'''
         self.id = id
-        self.tournament_name = tournament_name          # Name of the tournament
-        self.tournament_address = tournament_address    # Address of the tournament
+        self.tournament_name = name          # Name of the tournament
+        self.tournament_address = address    # Address of the tournament
         self.start_date = start_date                    # Starting date of the tournament
         self.end_date = end_date                        # End date of the tournament
         self.organizer = organizer
-        self.organizer_nr = organizer_nr
+        self.organizer_nr = organizer_number
 
     def __str__(self):
         '''Returns a string representation of the tournament object'''
